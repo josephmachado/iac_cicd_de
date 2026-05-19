92d4d26aa8c15eaf9dc576642f6bd7bd5a80f891
JosephKevinMachado <joseph.machado.professional@gmail.com>
2026-05-19 09:00:08 -0400

    revamp 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -116,6 +116,13 @@ resource "aws_instance" "this" {
     #!/bin/bash
     apt-get update -y
     apt-get install -y python3 python3-pip git
+
+    # install SSM agent
+    wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+    dpkg -i amazon-ssm-agent.deb
+    systemctl enable amazon-ssm-agent
+    systemctl start amazon-ssm-agent
+
     pip3 install boto3
   EOF
 
